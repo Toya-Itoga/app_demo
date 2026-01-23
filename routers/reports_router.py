00ef623b7b790dc53d5b58f702c0a758170f070d
糸賀透也 <suchmos@myMacbook.local>
2026-01-23 16:04:38 +0900

    feat: 日報登録画面の表示機能を追加

--- routers/reports_router.py
+++ routers/reports_router.py
@@ -1,6 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.templating import Jinja2Templates
 from services.reports_service import create_report_service
 
 
 
 router = APIRouter()
+templates = Jinja2Templates(directory="templates")
+
+
+@router.get("/create_report_form/{lot_id}")
+def create_report_form_show(request: Request, lot_id: str):
+    return templates.TemplateResponse(
+        "create_report_form.html",
+        {
+            "request": request,
+            "lot_id": lot_id
+        }
+    )
